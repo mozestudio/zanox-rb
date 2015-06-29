888f360a91b258b674dc64e6cd9e0d1c74dcba05
RoxasShadow <webmaster@giovannicapuano.net>
2015-06-29 17:18:19 +0200

    spec_helper: Disable debug

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -3,4 +3,3 @@ require 'rspec/collection_matchers'
 
 Zanox::API::Session.connect_id = '802B8BF4AE99EBE00F41'
 Zanox::API::Session.secret_key = 'fa4c0c2020Aa4c+ab9Ea0ec8d39E06/df2c5aa44'
-Zanox::API.debug!
